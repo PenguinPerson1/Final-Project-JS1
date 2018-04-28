449840a066a12ac085869ad466188a589432a913
Nia <nia.plossl@gmail.com>
2018-04-27 20:03:17 -0400

    A Previous Button

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,10 @@
+$("#Prev").hide();
+var previn1=0;
+var previn2=0;
+var previn3=0;
+var insult1=0;
+var insult2=0;
+var insult3=0;
 //arrays
 let insultsV1 =["artless ","bawdy ","beslubbering ","bootless ","churlish ","cockered ","clouted ","craven ","currish ","dankish ","dissembling ","droning ","errant ","fawning ","fobbing ","froward ","frothy ","gleeking ","goatish ","gorbellied ","impertinent ","infectious ","jarring ","loggerheaded ",
 "lumpish ","mammering ","mangled ","mewling ","paunchy ","pribbling ","puking ","puny ","qualling ","rank ","reeky ","roguish ","ruttish ","saucy ","spleeny ","spongy ","surly ","tottering ","unmuzzled ","vain ","venomed ","villainous ","warped ","wayward ","weedy ",
@@ -8,8 +15,6 @@ let insultsV2 =["base-court ","bat-fowling ","beef-witted ","beetle-headed ","bo
 let insultsV3 =["apple-john ","baggage ","barnacle ","bladder ","boar-pig ","bugbear ","bum-bailey ","canker-blossom ","clack-dish ","clotpole ","coxcomb ","codpiece ","death-token ","dewberry ","flap-dragon ","flax-wench ","flirt-gill ","foot-licker ","fustilarian ","giglet ","gudgeon ","haggard ","harpy ","hedge-pig ",
 "horn-beast ","hugger-mugger ","joithead ","lewdster ","lout ","maggot-pie ","malt-worm ","mammet ","measle ","minnow ","miscreant ","moldwarp ","mumble-news ","nut-hook ","pigeon-egg ","pignut ","puttock ","pumpion ","ratsbane ","scut ","skainsmate ","strumpet ","varlot ","vassal ","whey-face ",
 "wagtail "];
-
-
 $("#Submit").click(()=>{
   // adding more stuff
   let add1 = $("#input1").val();
@@ -25,16 +30,28 @@ $("#Submit").click(()=>{
   console.log(insultsV1[insultsV1.length]);
 });
 $("#G").click(() => {
+  previn1=insult1;
+  previn2=insult2;
+  previn3=insult3;
   $( "#Text0" ).empty();
-
   //random numbers
-  let insult1=Math.floor(Math.random()*insultsV1.length);
-  let insult2=Math.floor(Math.random()*insultsV2.length);
-  let insult3=Math.floor(Math.random()*insultsV2.length);
+  insult1=Math.floor(Math.random()*insultsV1.length);
+  insult2=Math.floor(Math.random()*insultsV2.length);
+  insult3=Math.floor(Math.random()*insultsV2.length);
 
   //showing results
   $("#Text0").append("Thou ");
   $("#Text0").append(insultsV1[insult1]);
   $("#Text0").append(insultsV2[insult2]);
   $("#Text0").append(insultsV3[insult3]);
+  $("#Prev").show();
+  //console.log(previn1);
+});
+$("#Prev").click(() =>  {
+  $("#Text0" ).empty();
+  $("#Text0").append("Thou ");
+  $("#Text0").append(insultsV1[previn1]);
+  $("#Text0").append(insultsV2[previn2]);
+  $("#Text0").append(insultsV3[previn3]);
+  $("#Prev").hide();
 });
